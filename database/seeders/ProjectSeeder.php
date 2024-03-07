33366f46a25ddbbdc84f43ea41d50cfff2eef121
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-03-07 17:18:09 +0100

    fix seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Controller\ProjectController;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
@@ -22,7 +23,7 @@ class ProjectSeeder extends Seeder
             $project->slug = Str::slug($project->title);
             $project->date = fake()->dateTime();
             $project->description = fake()->paragraph();
-            $project->type_id = fake()->paragraph();
+            $project->type_id = Type::inRandomOrder()->first()->id;
             $project->save();
         }
        
